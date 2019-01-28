0e887f17856f6d45bab598f521e9fdfa0fbd4f28
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2019-01-28 13:14:30 +0100

    Inline the data source config

--- src/main/scala/org/opencypher/grapddl/example/ldbc/LdbcExample.scala
+++ src/main/scala/org/opencypher/grapddl/example/ldbc/LdbcExample.scala
@@ -4,7 +4,8 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.internal.StaticSQLConf.CATALOG_IMPLEMENTATION
 import org.opencypher.grapddl.example.ldbc.DataImport._
 import org.opencypher.okapi.api.graph.Namespace
-import org.opencypher.spark.api.io.sql.IdGenerationStrategy
+import org.opencypher.spark.api.io.HiveFormat
+import org.opencypher.spark.api.io.sql.{IdGenerationStrategy, SqlDataSourceConfig}
 import org.opencypher.spark.api.{CAPSSession, GraphSources}
 
 object LdbcExample extends App {
@@ -20,20 +21,21 @@ object LdbcExample extends App {
   val sqlGraphSource = GraphSources
     .sql(resource("/ldbc/ddl/ldbc.ddl"))
     .withIdGenerationStrategy(IdGenerationStrategy.HashBasedId)
-    .withSqlDataSourceConfigs(resource("/ldbc/ddl/data-sources.json"))
+    .withSqlDataSourceConfigs(List(SqlDataSourceConfig(
+      dataSourceName = "warehouse", storageFormat = HiveFormat))
+    )
 
   // Register SQL PGDS
   session.catalog.register(Namespace("sql"), sqlGraphSource)
 
   // Run a Cypher query
   session.cypher(
-    s"""
-       |FROM GRAPH sql.LDBC
-       |MATCH (p:Person)-[:STUDYAT]->(u:University)
-       |WHERE u.name = 'National_Institute_of_Business_Management'
-       |RETURN p.firstName, u.name, u.url
-       |ORDER BY p.firstName
-     """.stripMargin).show
+    s"""FROM GRAPH sql.LDBC
+        MATCH (p:Person)-[:STUDYAT]->(u:University)
+        WHERE u.name = 'National_Institute_of_Business_Management'
+        RETURN p.firstName, u.name, u.url
+        ORDER BY p.firstName
+     """).show
 
 }
 
